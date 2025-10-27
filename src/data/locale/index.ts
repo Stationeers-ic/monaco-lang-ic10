88230f442367747ccc949b4bee0725de61fcba18
Kirill <traineratwot@yandex.ru>
2025-10-27 22:24:30 +0300

    1

--- src/data/locale/index.ts
+++ src/data/locale/index.ts
@@ -1,3 +1,4 @@
-import i18next from "i18next";
-export const i18n: ReturnType<typeof i18next.createInstance> = i18next.createInstance();
-export default i18n;
+import i18next from "i18next"
+import en from "./EN.json"
+export const i18n: ReturnType<typeof i18next.createInstance> = i18next.createInstance()
+export default i18n
